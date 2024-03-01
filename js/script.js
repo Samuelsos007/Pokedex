7bd8421905074a659bce80b98c71ac53784624df
Samuels007 <samueldasilvaoliveira17@gmail.com>
2024-03-01 02:39:21 -0300

    Finalização da Pokedex

--- js/script.js
+++ js/script.js
@@ -2,19 +2,37 @@ const pokemonName = document.querySelector('.pokemon__name');
 const pokemonNumber = document.querySelector('.pokemon__number');
 const pokemonImage = document.querySelector('.pokemon__image');
 const form = document.querySelector('.form');
+const input = document.querySelector('.input__search');
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
+
+let searchPokemon = 9;
 const fetchPokemon = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    if(APIResponse.status === 200){
+
     const data = await APIResponse.json();
     return data
+    }
 }
 
 const renderPokemon = async (pokemon) => {
-    const data = await fetchPokemon(pokemon);
+    pokemonName.innerHTML = 'Loading...';
+    pokemonNumber.innerHTML = '';
 
+    const data = await fetchPokemon(pokemon);
+    if(data){
+    pokemonImage.style.display = 'block';    
     pokemonName.innerHTML = data.name;
     pokemonNumber.innerHTML = data.id;
     pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
-
+    input.value = '';
+    searchPokemon = data.id;
+    }else {
+        pokemonImage.style.display = 'none';
+        pokemonName.innerHTML = 'Not found';
+        pokemonNumber.innerHTML = '';
+    }
 }
 
 form.addEventListener('submit', (event) => {
@@ -22,3 +40,18 @@ form.addEventListener('submit', (event) => {
     renderPokemon(input.value.toLowerCase());
     input.value = '';
 })
+
+buttonPrev.addEventListener('click', () => {
+    if(searchPokemon > 1){
+        searchPokemon -= 1;
+        renderPokemon(searchPokemon)
+    }
+})
+
+buttonNext.addEventListener('click', () => {
+    if(searchPokemon +=1){
+        renderPokemon(searchPokemon)
+    }
+})
+
+renderPokemon(searchPokemon)
